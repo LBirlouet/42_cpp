436580d42897c48c0501a5afeabcfa5d7f7b37f5
Lucas Birlouet <lbirloue@fu-r9-p4.s19.be>
2024-11-19 15:52:27 +0100

    fix : ex04

--- cpp_01/ex04/srcs/main.cpp
+++ cpp_01/ex04/srcs/main.cpp
@@ -3,68 +3,59 @@
 #include <sstream>
 
 int main(int ac, char **av){
+    std::ifstream myFile;
+    std::ofstream newFile;
+    std::string filename;
+    std::string newFilename;
+    std::string toReplace;
+    std::string replacement;
 
-std::string test1 = "bonjour ";
-std::string test2 = "toi ";
-std::string test3 = "nombril ";
+    std::stringstream buffer;
+    std::string fileContent;
+    std::string newfileContent;
+    size_t  i = 0;
+    size_t  j = 0;
 
+    if (ac != 4)
+        return 0;
+    filename = av[1];
+    toReplace = av[2];
+    replacement = av[3];
 
-std::ifstream myFile;
-std::ofstream newFile;
-std::string filename;
-std::string newFilename;
-std::string toReplace;
-std::string replacement;
+    myFile.open(filename.c_str());
 
-std::string fileContent;
-std::string newfileContent;
-size_t  i = 0;
-size_t  j = 0;
+    if (myFile.is_open())
+        ;
+    else{
+        std::cout << "Failed to open the file!" << std::endl;
+        return 0;
+    }
 
-if (ac != 4)
-    return 0;
-
-filename = av[1];
-toReplace = av[2];
-replacement = av[3];
-
-myFile.open(filename.c_str());
-
-if (myFile.is_open())
-    ;
-else{
-    std::cout << "Failed to open the file!" << std::endl;
-    return 0;
-}
-
-
-
-std::stringstream buffer;
     
-buffer << myFile.rdbuf();
-fileContent = buffer.str();
+    buffer << myFile.rdbuf();
+    fileContent = buffer.str();
 
-std::cout << "test" << fileContent << std::endl;
+    if (toReplace.length() > 0 && replacement.length() > 0){
+    while ((i = fileContent.find(toReplace, j)) != std::string::npos){
+        newfileContent.append(fileContent, j, i - j);
+        newfileContent.append(replacement);
+        j = i + toReplace.length();
+    }
+    newfileContent.append(fileContent, j, fileContent.length() - j);
+    }
 
-while ((i = fileContent.find(toReplace, j)) != std::string::npos)
-{
-    newfileContent.append(fileContent, j, i - j);
-    newfileContent.append(replacement);
-    j = i + toReplace.length();
-}
-newfileContent.append(fileContent, j, fileContent.length() - j);
+    newFilename = filename + ".replace";
 
-// std::cout << "test" << fileContent << std::endl;
+    newFile.open(newFilename.c_str());
 
+    if (newfileContent.length() > 0)
+        newFile << newfileContent;
+    else
+    {
+        std::cout<<"test" << fileContent <<std::endl;
+        newFile << fileContent;
+    }
 
-
-newFilename = filename + ".replace";
-
-newFile.open(newFilename.c_str());
-
-newFile << newfileContent;
-
-
-myFile.close();
-newFile.close();
+    myFile.close();
+    newFile.close();
 }
