ef4d226c99571a61081be018ec23c05cf319c9ae
Lucas Birlouet <lbirloue@fu-r10-p4.s19.be>
2024-12-03 15:05:54 +0100

    add : finish ex00

--- cpp_03/ex00/srcs/ClapTrap.cpp
+++ cpp_03/ex00/srcs/ClapTrap.cpp
@@ -15,34 +15,42 @@ ClapTrap::~ClapTrap(){
 }
 
 void ClapTrap::attack(const std::string& target){
-if (this->health > 0){
-    if (this->energyPoint > 0){
-        this->energyPoint--;
-        this->attackDamage++;
-        this->energyPoint--;
-        std::cout<<"ClapTrap "<< this->name <<" attacks "<< target <<", causing "<< this->attackDamage <<" points of damage!"""<<std::endl;
-    }
+    if (this->health > 0){
+        if (this->energyPoint > 0){
+            this->energyPoint--;
+            std::cout<<"ClapTrap "<< this->name <<" attacks "<< target <<", causing "<< this->attackDamage <<" points of damage!"""<<std::endl;
+        }
+        else
+            std::cout<<"ClapTrap "<< this->name <<" try to attack "<< target <<" but he has not enough energy points"<<std::endl;
+        }
     else
-        std::cout<<"ClapTrap "<< this->name <<"try to attack "<< target <<"but he has not enough energy points"<<std::endl;
-    }
-else
-    std::cout<<"ClapTrap "<< this->name <<"try to attack "<< target <<"but he's already dead"<<std::endl;
+     std::cout<<"ClapTrap "<< this->name <<" try to attack "<< target <<" but he's already dead"<<std::endl;
 }
 
 void ClapTrap::takeDamage(unsigned int amout){
-if ((this->health - amout) > 0){
-    this->health = this->health - amout;
-    std::cout<<"ClapTrap "<< this->name <<"take " << amout << " of damage, he has now" << this->health << " health points"<<std::endl;
+    if (this->health > (int)amout){
+        this->health = this->health - amout;
+        std::cout<<"AAAAAA ClapTrap "<< this->name <<" take " << amout << " of damage, he has now " << this->health << " health points"<<std::endl;
+    }
+    else if((this->health < (int)amout) && (this->health > 0)){
+        this->health = this->health - amout;
+        std::cout<<"ClapTrap "<< this->name <<" take " << amout << " of damage, now, he's dead"<<std::endl;
+    }
+    else
+        std::cout<<"ClapTrap "<< this->name <<" is already dead"<<std::endl;
+}
 
-}
-else if(((this->health - amout) <= 0) && (this->health > 0)){
-    this->health = this->health - amout;
-    std::cout<<"ClapTrap "<< this->name <<"take " << amout << " of damage, now, he's dead"<<std::endl;
-}
-else
-    std::cout<<"ClapTrap "<< this->name <<"is already dead"<<std::endl;
-}
 
 void ClapTrap::beRepaired(unsigned int amout){
-
+    if (this->health > 0){
+        if (this->energyPoint > 0){
+            this->energyPoint--;
+            this->health+= amout;
+            std::cout<<"ClapTrap "<< this->name <<" repaired himself "<< amout <<" health points, he has now " << this->health << " health points"<<std::endl;
+        }
+        else
+            std::cout<<"ClapTrap "<< this->name <<" try to repaired himself "<< amout <<" health points but he has not enough energy points"<<std::endl;
+        }
+    else
+        std::cout<<"ClapTrap "<< this->name <<" try to repaired himself "<< amout <<" health points but he's already dead"<<std::endl;
 }
--- cpp_03/ex00/srcs/main.cpp
+++ cpp_03/ex00/srcs/main.cpp
@@ -1,5 +1,23 @@
 #include "../includes/ClapTrap.hpp"
 
 int main(){
-    ClapTrap player1("Nicolas");
+    ClapTrap player("Nicolas");
+
+    player.attack("Charlie");
+    player.beRepaired(15);
+    player.takeDamage(24);
+    player.beRepaired(2);
+    player.takeDamage(4);
+    player.beRepaired(10);
+    player.attack("Charlie");
+
+    std::cout << "*****************************************" << std::endl;
+    
+    ClapTrap player2("Victor");
+
+    for (int i = 0; i < 10; i++){
+        player2.attack("Julien");
+    }
+    player2.attack("Julien");
+    player2.beRepaired(5);
 }
