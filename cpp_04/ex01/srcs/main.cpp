2114582fdd82e9b575d7956acf238b7a3f7b875a
Lucas Birlouet <lbirloue@fu-r10-p4.s19.be>
2024-12-12 12:57:01 +0100

    clear

--- cpp_04/ex01/srcs/main.cpp
+++ cpp_04/ex01/srcs/main.cpp
@@ -7,42 +7,8 @@
 
 int	main()
 {
-	// const Animal* meta = new Animal();
-	// const Animal* j = new Dog();
-	// const Animal* i = new Cat();
-
-	// std::cout << j->getType() << " " << std::endl;
-	// std::cout << i->getType() << " " << std::endl;
-
-	// i->makeSound();
-	// j->makeSound();
-	// meta->makeSound();
-
-	// delete i;
-	// delete j;
-	// delete meta;
-
-	// std::cout << "\n******************************\n" << std ::endl;
-
-	// const WrongAnimal	*k = new WrongAnimal();
-	// const WrongCat		*l = new WrongCat();
-	// const WrongAnimal	*m = new WrongCat();
-
-	// std::cout << k->getType() << " " << std::endl;
-	// std::cout << l->getType() << " " << std::endl;
-	// std::cout << m->getType() << " " << std::endl;
-
-	// k->makeSound();
-	// l->makeSound();
-	// m->makeSound();
-
-	// delete k;
-	// delete l;
-	// delete m;
-
     Animal* animals[6];
 
-    // Remplir le tableau avec des Dogs et des Cats
     for (int i = 0; i < 3; i++) {
         animals[i] = new Dog();
     }
@@ -50,15 +16,11 @@ int	main()
         animals[i] = new Cat();
     }
 
-    // Faire du bruit avec chaque animal
     for (int i = 0; i < 6; i++) {
         animals[i]->makeSound();
     }
 
-    // Supprimer chaque animal
     for (int i = 0; i < 6; i++) {
         delete animals[i];
     }
-
-
 }
