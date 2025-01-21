c0c8abfc0619cedffa6ddfb56046d392029cb5da
Lucas Birlouet <lucasbirlouer@MacBook-Air-de-Lucas.local>
2025-01-21 16:15:45 +0100

    add: more in cpp06-ex00

--- cpp_06/ex00/includes/ScalarConverter.hpp
+++ cpp_06/ex00/includes/ScalarConverter.hpp
@@ -2,7 +2,28 @@
 #define SCALARCONVERTER_HPP
 
 #include "iostream"
+#include "cmath"
 
+class ScalarConverter{
 
+public:
+
+    ScalarConverter();
+    ScalarConverter(const std::string& param);
+    ScalarConverter(const ScalarConverter& cp);
+    ScalarConverter& operator=(const ScalarConverter& cp);
+    ~ScalarConverter();
+
+    void    conv();
+    char    convChar();
+    int     convInt();
+    float   convFloat();
+    double  convDouble();
+
+private:
+
+     std::string    str;
+
+};
 
 #endif
--- cpp_06/ex00/srcs/ScalarConverter.cpp
+++ cpp_06/ex00/srcs/ScalarConverter.cpp
@@ -1,1 +1,24 @@
-#includes "../includes.ScarlarConverter.hpp"
+#include "../includes/ScarlarConverter.hpp"
+
+// default constructor (Orthodox Canonical Form)
+ScarlarConverter::ScarlarConverter(){
+    std::cout<<"[ScarlarConverter] default constructor called" <<std::endl;
+}
+
+// copy constructor (Orthodox Canonical Form)
+ScarlarConverter::ScarlarConverter(const ScarlarConverter& cp){
+    std::cout<<"[ScarlarConverter] Copy constructor called"<<std::endl;
+    *this = cp;
+}
+
+// assignment operator (Orthodox Canonical Form)
+ScarlarConverter& ScarlarConverter::operator=(const ScarlarConverter& cp){
+	(void)cp;
+    std::cout << "[Bureaucrat] Copy assignment operator called" << std::endl;
+	return *this;
+}
+
+// default destructor (Orthodox Canonical Form)
+ScarlarConverter::~ScarlarConverter(void){
+    std::cout<<"[ScarlarConverter] default destructor called" <<std::endl;
+}
