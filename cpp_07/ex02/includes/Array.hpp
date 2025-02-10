2dcd66a2eadc972bab8497ed03a22756b9d3a2e9
Lucas Birlouet <lucasbirlouer@MacBook-Air-de-Lucas.local>
2025-02-10 12:18:31 +0100

    add : finishing v1.0 cpp07/ex02

--- cpp_07/ex02/includes/Array.hpp
+++ cpp_07/ex02/includes/Array.hpp
@@ -3,6 +3,66 @@
 
 #include "iostream"
 
+template <typename T>
+class Array{
+
+public:
+    //default constructor
+    Array() : arraySize(0), data(NULL){
+    }
+    //constructor with arg
+    Array(unsigned int n) : arraySize(n){
+        data = new T[n]();
+    }
+
+    //cp constructor
+    Array(const Array& cp) : arraySize(cp.arraySize){
+        data = new T[arraySize];
+        for (unsigned int i = 0; i < arraySize; ++i)
+            data[i] = cp.data[i];
+    }
+
+    //assignement operator
+    Array& operator=(const Array& cp){
+        if (this != &cp){
+            delete[] data;
+            arraySize = cp.arraySize;
+            data = new T[arraySize];
+            for (unsigned int i = 0; i < arraySize; ++i)
+                data[i] = cp.data[i];
+        }
+        return *this;
+    }
+    //destructor
+    ~Array(){
+        delete[] data;
+    }
+
+    //overload operator[]
+    T& operator[](int index) {
+        if (index < 0 || static_cast<unsigned int>(index) >= arraySize) {
+            throw std::out_of_range("Index out of bounds");
+        }
+        return data[index];
+    }
+
+    //overload operator[] for const
+    const T& operator[](int index) const{
+        if (index < 0 || static_cast<unsigned int>(index) >= arraySize)
+            throw std::out_of_range("Index out of bounds");
+        return data[index];
+    }
+
+    //recover array size
+    unsigned int size() const{
+        return (arraySize);
+    }
+
+private:
+    unsigned int arraySize;
+    T* data;
+
+};
 
 
 
--- cpp_07/ex02/srcs/main.cpp
+++ cpp_07/ex02/srcs/main.cpp
@@ -1,5 +1,84 @@
+#include <iostream>
 #include "../includes/Array.hpp"
 
-int main(){
-    
+#define MAX_VAL 750
+
+int main(int, char**)
+{
+    std::cout << "======PERSONNAL MAIN======" << std::endl;
+    /*---------PERSONNAL MAIN---------*/
+
+    Array<int> emptyArray;
+    std::cout << "Size of empty array: " << emptyArray.size() << std::endl;
+
+    Array<int> simpleArray(3);
+    simpleArray[0] = 10;
+    simpleArray[1] = 20;
+    simpleArray[2] = 30;
+    std::cout << "data in simpleArray: ";
+    for (unsigned int i = 0; i < simpleArray.size(); ++i)
+        std::cout << simpleArray[i] << " ";
+    std::cout << std::endl;
+
+    Array<int> copyArray = simpleArray;
+    copyArray[0] = 100;
+    std::cout << "simpleArray[0]: " << simpleArray[0] << ", copyArray[0]: " << copyArray[0] << std::endl;
+
+    Array<std::string> stringArray(2);
+    stringArray[0] = "Hello";
+    stringArray[1] = "World";
+    std::cout << "data in stringArray: ";
+    for (unsigned int i = 0; i < stringArray.size(); ++i)
+        std::cout << stringArray[i] << " ";
+    std::cout << std::endl;
+
+std::cout<<"======SUBJECT MAIN======"<<std::endl;
+/*---------SUBJECT MAIN---------*/
+
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
 }
