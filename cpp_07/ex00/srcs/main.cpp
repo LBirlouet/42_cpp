c10667447d3f692200e686f79b00f790bdb53521
Lucas Birlouet <lbirloue@mi-r1-p4.s19.be>
2025-02-19 09:23:17 +0100

    add : add some exemple in main

--- cpp_07/ex00/srcs/main.cpp
+++ cpp_07/ex00/srcs/main.cpp
@@ -3,7 +3,12 @@
 int main(){
     int i = 10;
     int j = 20;
+    char aa = 'a';
+    char bb = 'b';
 
+/* WHIT INT */
+    std::cout << "---------------" << std::endl;
+    std::cout << "---WHIT INT----" << std::endl;
     std::cout << "---------------" << std::endl;
     std::cout << "MIN : " << min(i,j) << std::endl;
     std::cout << "MAX : " << max(i,j) << std::endl;
@@ -18,8 +23,31 @@ int main(){
     std::cout << "After swap :" << std::endl;
     std::cout << "I : " << i << std::endl;
     std::cout << "J : " << j << std::endl;
+
+/* WHIT CHAR */
+
+    std::cout << "---------------" << std::endl;
+    std::cout << "---WHIT CHAR---" << std::endl;
+    std::cout << "---------------" << std::endl;
+    std::cout << "MIN : " << min(aa,bb) << std::endl;
+    std::cout << "MAX : " << max(aa,bb) << std::endl;
+    std::cout << "---------------" << std::endl;
+    std::cout << "Before swap :" << std::endl;
+    std::cout << "I : " << aa << std::endl;
+    std::cout << "J : " << bb << std::endl;
+    std::cout << "---------------" << std::endl;
+
+    ::swap(aa,bb);
+
+    std::cout << "After swap :" << std::endl;
+    std::cout << "A : " << aa << std::endl;
+    std::cout << "B : " << bb << std::endl;
+
+/* SUBJECT MAIN */
+
+
     std::cout << "---------------" << std::endl << std ::endl;
-    std::cout << "SUBJECT MAIN" << std::endl;
+    std::cout << "-SUBJECT MAIN--" << std::endl;
     std::cout << "---------------" << std::endl;
 
     int a = 2;
